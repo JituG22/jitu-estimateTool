83d4574d5fb162f391cc519ff3dc5c2d23277644
jitu <jitu123435@gmail.com>
2021-08-31 20:02:02 +0530

    login button changes

--- src/login/login.js
+++ src/login/login.js
@@ -32,7 +32,7 @@ export default function Login() {
                                         <TextField id="pass" type="password" placeholder="password" size="small" variant="outlined" />
                                         </FormControl>
 
-                                        <Button variant="contained" color="primary"  type="submit"> Login </Button>
+                                        <Button variant="contained" color="primary"  type="submit"> Login here </Button>
                                 </form>
             </Grid>
         </Grid>
